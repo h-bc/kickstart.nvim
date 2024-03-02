1c1b80d6de3c2679d3e66c1240d51b5ef06d6420
Hakim BC <h.bc@outlook.com>
2024-03-02 19:33:38 +0000

    make cursor block for all modes

--- init.lua
+++ init.lua
@@ -103,7 +103,7 @@ vim.opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
-
+vim.opt.guicursor = 'n-v-i-c:block-Cursor'
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
 
